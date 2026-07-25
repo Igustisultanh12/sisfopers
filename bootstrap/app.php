1289d60c884d9277a3a5a0a66b151fd19280e663
Igustisultanh12 <91646835+Igustisultanh12@users.noreply.github.com>
2026-07-25 20:46:13 +0700

    feat: append LogControllerActivity middleware globally in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,6 +30,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             HandleInertiaRequests::class,
             \App\Http\Middleware\DetectMobileDeviceMiddleware::class,
+            \App\Http\Middleware\LogControllerActivity::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
