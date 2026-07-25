67320fdf4c4e9a0a980d0f7776cdabb5064af2a6
Igustisultanh12 <91646835+Igustisultanh12@users.noreply.github.com>
2026-07-25 20:56:48 +0700

    fix: add skep.check and skep.request public routes to routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,10 @@ Route::get('/', function () {
 // PORTAL VERIFIKASI DOKUMEN PUBLIK (Bisa di-scan via HP tanpa login)
 Route::get('/verify-doc/{verify_code}', [\App\Http\Controllers\Public\DocumentVerificationController::class, 'show'])->name('public.verify-doc');
 
+// Rute Pengecekan NIKC & Pengajuan SKEP Publik (Digunakan Saat Form Registrasi)
+Route::post('/skep/check', [SkepPublicController::class, 'checkNikc'])->name('skep.check');
+Route::post('/skep/request', [SkepPublicController::class, 'submitRequest'])->name('skep.request');
+
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login']);
