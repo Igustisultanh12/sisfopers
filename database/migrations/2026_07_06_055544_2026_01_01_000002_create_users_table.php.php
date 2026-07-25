f906c93ff084e91f0b2279a00b6c106031460cab
Igustisultanh12 <91646835+Igustisultanh12@users.noreply.github.com>
2026-07-25 12:09:06 +0700

    Sync database/migrations/2026_07_06_055544_2026_01_01_000002_create_users_table.php.php from sisfopers1

---
+++ database/migrations/2026_07_06_055544_2026_01_01_000002_create_users_table.php.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->foreignId('role_id')->constrained('roles')->onDelete('restrict');
+            $table->string('username', 50)->unique()->index();
+            $table->string('email', 100)->unique()->index();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->boolean('is_active')->default(false)->index();
+            $table->boolean('two_factor_enabled')->default(false);
+            $table->text('two_factor_secret')->nullable();
+            $table->text('two_factor_recovery_codes')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
