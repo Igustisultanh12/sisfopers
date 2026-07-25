bba24cb377bf51885d5ad75ef5c7e05fbb2375b2
Igustisultanh12 <91646835+Igustisultanh12@users.noreply.github.com>
2026-07-25 20:24:42 +0700

    refactor: revert upload disk to local default

--- app/Http/Controllers/Admin/MasterPersonelController.php
+++ app/Http/Controllers/Admin/MasterPersonelController.php
@@ -233,11 +233,7 @@ class MasterPersonelController extends Controller
             ]);
 
             // Standard Laravel upload (Disimpan di disk private aman)
-            try {
-                $fileName = $request->file('photo_profile')->store('personel/photos', 'private');
-            } catch (\Exception $e) {
-                $fileName = $request->file('photo_profile')->store('personel/photos', 'local');
-            }
+            $fileName = $request->file('photo_profile')->store('personel/photos', 'local');
 
             $otpCode   = str_pad(strval(rand(0, 999999)), 6, '0', STR_PAD_LEFT);
             $expiredAt = now()->addHours(72);
@@ -378,11 +374,7 @@ class MasterPersonelController extends Controller
                     Storage::disk('private')->delete($personel->photo_profile);
                     Storage::disk('public')->delete($personel->photo_profile);
                 }
-                try {
-                $fileName = $request->file('photo_profile')->store('personel/photos', 'private');
-            } catch (\Exception $e) {
                 $fileName = $request->file('photo_profile')->store('personel/photos', 'local');
-            }
                 $personel->photo_profile = $fileName;
             }
 
